32da16600304974affa377874b53353874dc1c31
Haein2809 <50579220+Haein2809@users.noreply.github.com>
2024-08-22 23:52:36 +0900

    Create 002_create_table.sql

---
+++ database/migrations/002_create_table.sql
@@ -0,0 +1,16 @@
+-- mindbridge DB에 접속하여 테이블 생성
+\c mindbridge
+
+CREATE TABLE mental_depress_sym (
+    id SERIAL PRIMARY KEY,
+    region_name VARCHAR(100),
+    year INT,
+    prevalence_rate DECIMAL(5,2)
+);
+
+CREATE TABLE mental_depres_experience (
+    id SERIAL PRIMARY KEY,
+    region_name VARCHAR(100),
+    year INT,
+    experience_rate DECIMAL(5,2)
+);
